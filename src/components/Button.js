e17cbb9a03452564ed5da2446f770be902f0a38f
Megan Penn <meganpenn@Megans-MacBook-Pro-2.local>
2024-03-20 09:19:55 -0400

    Button tab failure

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 export const Button = (text) => {
   return (
-    <button className="button" type="button" onClick={() => console.log("-- Click")}>{text}</button>
+    <button className="button" type="button" onClick={() => console.log("-- Click")} tabIndex="1">{text}</button>
   )
 }
