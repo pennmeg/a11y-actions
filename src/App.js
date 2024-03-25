9be7bc0ea1aa41ea526c81beffa9c2ff70d7055b
Megan Penn <meganpenn@Megans-MacBook-Pro-2.local>
2024-03-25 15:50:48 -0400

    Add comments and notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from './components/Button';
-import { CustomImage } from './components/CustomImage';
 
 function App() {
   return (
@@ -11,16 +10,21 @@ function App() {
       <h2>Images and Alterative Text</h2>
       <div className="image-wrapper">
         <img src={logo} className="image-logo" alt="Logo" />
-        <CustomImage path={logo} />
+        {/* TO DO: Building a custom component to manage config linter ruleset */}
+        {/* <CustomImage path={logo} /> */}
       </div>
       <h2>Forms and Inputs</h2>
       {/* Interesting that this doesn't produce a linting error as there is no ID/for between the label and the input */}
+      {/* Docs say that it does look for inputs without labels and there is nothing explicit tying these two together */}
+      {/* This does fail the browser extension scan test */}
       <div className="form-label-group">
         <label>First Name</label>
-        <input type="text"></input>
+        <input type="text" />
       </div>
       <h2>Buttons</h2>
-      <Button text={"Click Me"}></Button>
+      <Button text="Click Me"></Button>
+      {/* VS Code Linter doesn't appear to find issues like the one below */}
+      {/* This should have a tabindex on it so that is is focusable */}
       <div className="button" role="button" onClick={() => console.log("-- Click")}>Click Me: Div</div>
     </div>
   );
